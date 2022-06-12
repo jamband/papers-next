06c1b27a64f4b0c8d905e1876592fe01de419d42
Tomoki Morita <tmsongbooks215@gmail.com>
2022-06-12 13:24:05 +0900

    Add some scripts

---
+++ scripts/clean.mjs
@@ -0,0 +1,11 @@
+import { rmSync } from "fs";
+
+/** @type {import("fs").RmOptions} */
+const options = {
+  recursive: true,
+  force: true,
+};
+
+rmSync(".next", options);
+rmSync("node_modules", options);
+rmSync("tsconfig.tsbuildinfo", options);
