f4a5f8ab9b3427d8b7f6022c7efbee39d8db4809
Tomoki Morita <tmsongbooks215@gmail.com>
2022-12-17 17:49:18 +0900

    update dependencies
    and fix some types for react hook form

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,10 +1,18 @@
-import type { FieldValues, Path, UseFormSetError } from "react-hook-form";
+import type {
+  FieldValues,
+  Path,
+  UnPackAsyncDefaultValues,
+  UseFormSetError,
+} from "react-hook-form";
 
 export const setErrors = <T extends FieldValues>(
   errors: Record<string, string>,
   setError: UseFormSetError<T>
 ) => {
   for (const [name, message] of Object.entries(errors)) {
-    setError(name as Path<T>, { type: "server", message });
+    setError(name as Path<UnPackAsyncDefaultValues<T>>, {
+      type: "server",
+      message,
+    });
   }
 };
