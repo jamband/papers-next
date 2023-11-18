6db509ef961642c8eb21803571a1afeea6617a21
Tomoki Morita <tmsongbooks215@gmail.com>
2023-11-18 16:13:51 +0900

    add some hooks for require

--- app/_hooks/require.ts
+++ app/_hooks/require.ts
@@ -3,6 +3,47 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuth } from "./auth";
 
+export const useRequireGuest = () => {
+  const { auth } = useAuth();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (auth === undefined) {
+      return;
+    }
+
+    if (auth?.role === "admin") {
+      push("/admin");
+      return;
+    }
+
+    if (auth) {
+      push("/");
+      return;
+    }
+  }, [auth, push]);
+};
+
+export const useRequireAuth = () => {
+  const { auth } = useAuth();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (auth === undefined) {
+      return;
+    }
+
+    if (auth === null) {
+      push("/login");
+      return;
+    }
+
+    if (auth.role === "admin") {
+      push("/admin");
+    }
+  }, [auth, push]);
+};
+
 export const useRequireVerified = () => {
   const { auth } = useAuth();
   const { push } = useRouter();
@@ -24,6 +65,22 @@ export const useRequireVerified = () => {
 
     if (!auth.is_verified) {
       push("/verify-email");
+      return;
+    }
+  }, [auth, push]);
+};
+
+export const useRequireAdmin = () => {
+  const { auth } = useAuth();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (auth === undefined) {
+      return;
+    }
+
+    if (auth?.role !== "admin") {
+      push("/");
       return;
     }
   }, [auth, push]);
