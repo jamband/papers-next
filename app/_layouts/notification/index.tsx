54698926a3235b00b06cbf2588117c1accebbe94
Tomoki Morita <tmsongbooks215@gmail.com>
2023-11-20 15:58:52 +0900

    change notification component to toast-like style

--- app/_layouts/notification/index.tsx
+++ app/_layouts/notification/index.tsx
@@ -4,27 +4,20 @@ import {
   useNotificationAction,
   useNotificationState,
 } from "@/_hooks/notification";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
 import { Component } from "./component";
 import type { Props } from "./types";
 
 export const Notification: React.FC<Props> = (props) => {
   const { message, color } = useNotificationState();
   const { clear } = useNotificationAction();
-  const pathname = usePathname();
 
-  useEffect(() => {
-    if (!message) {
-      return;
-    }
-
-    clear();
-  }, [pathname]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  if (!message) {
+  if (message === "") {
     return null;
   }
+
+  setTimeout(() => {
+    clear();
+  }, 5000);
 
   return <Component {...props} message={message} clear={clear} color={color} />;
 };
