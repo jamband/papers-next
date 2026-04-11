0c9fb2286d88fd340d8e7d4b376d3b55b91fd037
Tomoki Morita <tmsongbooks215@gmail.com>
2026-04-11 14:03:18 +0900

    improve some styles

--- app/_components/form/check/component.tsx
+++ app/_components/form/check/component.tsx
@@ -5,17 +5,19 @@ import type { _Props } from "./types";
 
 export const Component: React.FC<_Props> = (props) => (
   <fieldset className={`${styles.container} ${props.className || ""}`}>
-    <input
-      type="checkbox"
-      name={props.name}
-      id={props.id}
-      className={`${props.inputClass || ""} ${
-        props.feedback ? styles.feedback : ""
-      }`}
-      aria-describedby={`${props.id}-feedback`}
-      defaultChecked={props.defaultChecked || false}
-    />
-    <FormLabel htmlFor={props.id}>{props.label}</FormLabel>
+    <div className={styles.main}>
+      <input
+        type="checkbox"
+        name={props.name}
+        id={props.id}
+        className={`${props.inputClass || ""} ${
+          props.feedback ? styles.feedback : ""
+        }`}
+        aria-describedby={`${props.id}-feedback`}
+        defaultChecked={props.defaultChecked || false}
+      />
+      <FormLabel htmlFor={props.id}>{props.label}</FormLabel>
+    </div>
     {!!props.feedback && (
       <FormFeedback id={`${props.id}-feedback`} message={props.feedback} />
     )}
